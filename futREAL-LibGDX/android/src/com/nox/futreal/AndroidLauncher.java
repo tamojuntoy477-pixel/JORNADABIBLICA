c89460cfd3e5a1d2f633ea3be39b676d49e14bb7
tamojuntoy477-pixel <tamojuntoy477@gmail.com>
2026-09-10 09:14:50 -0300

    feat: launch futREAL v11 rebuild

--- futREAL-LibGDX/android/src/com/nox/futreal/AndroidLauncher.java
+++ futREAL-LibGDX/android/src/com/nox/futreal/AndroidLauncher.java
@@ -15,6 +15,6 @@ public class AndroidLauncher extends AndroidApplication {
         config.useCompass = false;
         config.useGyroscope = false;
         config.numSamples = 4;
-        initialize(new FutRealV10Game(), config);
+        initialize(new FutRealV11Game(), config);
     }
 }
